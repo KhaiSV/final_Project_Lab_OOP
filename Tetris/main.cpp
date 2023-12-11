7fcbe2a58d503ea284c10fed464b51e04a51b046
KhaiSV <22127174@student.hcmus.edu.vn>
2023-12-11 17:20:36 +0700

    Add project files.

---
+++ Tetris/main.cpp
@@ -0,0 +1,45 @@
+﻿#include "header.h"
+#include "board.h"
+
+int main() {
+	Board b;
+	b.ShowBorder();
+	Coordinates cMove(11, top + 1);
+	PieceO p1;
+	p1.Show();
+
+	time_t originalTime = time(0);
+	char keyboard = 'a';
+	while (true) {
+		p1.MoveDownTime(b, originalTime);
+		//p1.Show();
+		if (kbhit()) {
+			keyboard = getch();
+
+			if (keyboard == 'm' && p1.RotateCheck(b, 1)) {
+				p1.RotateRight();
+				p1.Show();
+			}
+			else if (keyboard == 'n' && p1.RotateCheck(b, 3)) {
+				p1.RotateLeft();
+				p1.Show();
+			}
+			else if (keyboard == 'd') {
+				p1.MoveRight(b);
+				p1.Show();
+			}
+			else if (keyboard == 'a') {
+				p1.MoveLeft(b);
+				p1.Show();
+			}
+			else if (keyboard == 's') {
+				p1.MoveDownTime(b, originalTime);
+				p1.Show();
+			}
+			else if (keyboard == 'b') {
+				break;
+			}
+		}
+	}
+	return 0;
+}
